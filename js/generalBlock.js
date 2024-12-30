ba2d4dd684cd66dfb14ca2f0b140512257616b7f
Василь Горюк <61389266+vargoscreator@users.noreply.github.com>
2024-12-30 23:30:33 +0200

    add adaptive
    адаптив страниц плюс фиксы

---
+++ js/generalBlock.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const generalBlockInner = document.querySelector(".generalBlock__inner");
+    const selectBlock = document.querySelector(".generalBlock__select");
+    const contentBlock = document.querySelector(".generalBlock__content");
+    
+    const moveSelectBlock = () => {
+        if (window.innerWidth < 768) {
+            if (!contentBlock.contains(selectBlock)) {
+                contentBlock.insertBefore(selectBlock, contentBlock.firstChild);
+            }
+        } else {
+            generalBlockInner.insertBefore(selectBlock, generalBlockInner.firstChild);
+        }
+    };
+    moveSelectBlock();
+
+    window.addEventListener("resize", moveSelectBlock);
+});
