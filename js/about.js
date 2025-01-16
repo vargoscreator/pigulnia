db2d251cfc411da4ded9260abca8d66f1e35c749
Василь Горюк <61389266+vargoscreator@users.noreply.github.com>
2025-01-16 10:39:25 +0200

    fix slider

--- js/about.js
+++ js/about.js
@@ -1,4 +1,5 @@
-let certificatesSwiper = new Swiper(".certificates__slider", {
+let certificatesSwiper;
+certificatesSwiper = new Swiper('.certificates__slider', {
     loop: true,
     slidesPerView: 1,
     spaceBetween: 35,
@@ -7,4 +8,26 @@ let certificatesSwiper = new Swiper(".certificates__slider", {
         nextEl: ".certificates__slide-next",
         prevEl: ".certificates__slide-prev",
     },
+    on: {
+        slideChange: updateActiveDownloadLink,
+    },
 });
+
+// Функция для обновления класса active
+function updateActiveDownloadLink() {
+    if(certificatesSwiper){
+        const activeIndex = certificatesSwiper.realIndex; // Получаем индекс текущего слайда
+        const downloadLinks = document.querySelectorAll('.certificates__slide-download');
+    
+        // Убираем класс active у всех ссылок
+        downloadLinks.forEach(link => link.classList.remove('active'));
+    
+        // Добавляем класс active к соответствующей ссылке
+        if (downloadLinks[activeIndex]) {
+            downloadLinks[activeIndex].classList.add('active');
+        }
+    }
+}
+
+// Первоначальное обновление
+updateActiveDownloadLink();
